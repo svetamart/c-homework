9b9763276d8c47863f57a3d228229bdcb8c2a0bc
Sveta Martynova <svetamart95@gmail.com>
2022-08-23 19:11:10 +0300

    renamed task 1 now task 2

---
+++ task 1/task 2.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число a");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+    Console.Write("max = ");
+    Console.Write(max);
+}
