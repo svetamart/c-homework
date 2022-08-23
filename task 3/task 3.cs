e7438e4e1d59dd3d92ea16ed81e9292bd73c0db9
Sveta Martynova <svetamart95@gmail.com>
2022-08-23 18:25:13 +0300

    modified task 3

--- task 3/task 3.cs
+++ task 3/task 3.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
