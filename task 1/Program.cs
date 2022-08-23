ec67916bb2f2796220baceb71192aa3224691a31
Sveta Martynova <svetamart95@gmail.com>
2022-08-23 18:02:58 +0300

    added task 1

---
+++ task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+    Console.WriteLine("max = ");
+    Console.WriteLine(max);
+}
