e7459c40fd0ac5406fb3bfc4bd01212cc551cab0
Sveta Martynova <svetamart95@gmail.com>
2022-08-23 18:15:57 +0300

    added task 2

--- task 2/task 2.cs
+++ task 2/task 2.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+max = c;
+}
+Console.Write("max = ");
+Console.Write(max);
